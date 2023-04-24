cd45db15e5b5f40b8b3699c6412b0734d8354b3a
rukia <ruqayashar1@gmail.com>
2023-04-24 23:19:21 +0300

    complete

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-function MovieCard({title,posterSrc,genres})
-  
- {
+function MovieCard({
+  title,
+  genres,
+  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+}) {
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
@@ -11,5 +13,4 @@ function MovieCard({title,posterSrc,genres})
     </div>
   );
 }
-
 export default MovieCard;
